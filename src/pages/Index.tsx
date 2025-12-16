4da29f8d061a0a1952281ce31134cf44b84c16b6
Antony <antonyluke.p2024aids@sece.ac.in>
2025-12-16 12:11:38 +0530

    final2

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,7 @@
 import { SplineSceneBasic } from "@/components/SplineSceneBasic";
 import { Spotlight } from "@/components/ui/spotlight";
 import  Navbar  from "@/components/Navbar";
-import About from "@/components/about";
+import About from "@/components/About";
 import { CursorGlow } from "@/components/ui/cursor-glow";
 import { VantaBackground } from "@/components/ui/vanta-background";
 import Theme from "@/components/Theme";
